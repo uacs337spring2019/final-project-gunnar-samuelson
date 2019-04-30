020c82c4451a08e5d8251989296f880a23907737
gunngunn <gunnarsamuelson@yahoo.com>
2019-04-29 20:16:53 -0700

    Add files via upload

---
+++ stock337.js
@@ -0,0 +1,68 @@
+//Gunnar Samuelson, CSC Final Project, stock337.js
+//The inspiration for this program came from my love for the stock market. I wanted to
+// make an app that would easily pull up some useful and basic information about a 
+//stock that you type in the search bar. I then have a watch list that you can add your 
+//favorite stocks too and compare them in the future. The back end is hooked up to teh html page
+//so I can easily grab and manipulate variables on the front end.
+
+
+window.onload = function() {
+	document.getElementById("search").onclick = submit;
+	
+}
+
+function submit() {
+	let stockID = document.getElementById("ticker").value;
+	let url = "http://localhost:3000?stock=" + stockID;
+	fetch(url)
+		.then(checkStatus)
+		.then(function(responseText) {
+			let checker = JSON.parse(responseText);
+			let formatted = checker.split("\t");
+			let worked = formatted.slice(-7,-1);
+			console.log(worked)
+			postToPage(worked);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+}
+
+function postToPage(tick) {
+	let postToBodyComm = document.getElementById("stockInfo");
+	let watchlistDiv = document.getElementById("watchlist");
+	let company = "Company: " + tick[0];
+	let price = "Price: $" + tick[1];
+	let mktCap = "Market Cap: " + tick[2];
+	let IPO = "IPO Year: " + tick[3];
+	let sector = "Sector: " + tick[4];
+	let core = "Core Business: " + tick[5];
+	let finalPost = "<br><br>" + company + "<br><br>" + price + "<br><br>" + mktCap + "<br><br>" + IPO + "<br><br>" + sector + "<br><br>" + core;
+	console.log("Company: " + company)
+	console.log("Price: " + price)
+	console.log("Market Cap: " + mktCap) 
+	console.log("IPO Year: " + IPO)
+	console.log("Sector: " + sector)
+	console.log("Core Business: " + core)
+	postToBodyComm.innerHTML = finalPost;
+	document.getElementById("addWatch").onclick = function(){
+		watchlistDiv.innerHTML += "<br>" + "- " + tick[0] + ":\t" + "$" + tick[1] + "<br>"};
+
+}
+
+
+
+
+
+
+function checkStatus(response) {  
+    if (response.status >= 200 && response.status < 300) {  
+        return response.text();
+    } else if (response.status == 404) {
+    	// sends back a different error when we have a 404 than when we have
+    	// a different error
+    	return Promise.reject(new Error("Sorry, we couldn't find that page")); 
+    } else {  
+        return Promise.reject(new Error(response.status+": "+response.statusText)); 
+    } 
+}
---
+++ stock337_service.js
@@ -0,0 +1,75 @@
+//Gunnar Samuelson, CS 337, stock337_service.js
+//THis is my service file where I creeate and host my server with all of my data on it. THe data
+//is stored in the same directory as this file. I used some of the logic from almost all of our
+//past projects to help get this up and running properly. Mainly from the gerrymandering as that 
+//prject relied heavily on using data from a server as well as setting up the server in the 
+//first place.
+
+
+const express = require("express");
+const app = express();
+var fs = require("fs");
+
+app.use(express.static('public'));
+
+function read_file(file_name) {
+	var stocksFile = 0;
+	try {  
+	    stocksFile = fs.readFileSync(file_name, 'utf8');
+	    console.log(stocksFile);    
+	} catch(e) {
+	    console.log('Error:', e.stack);
+	}
+	console.log(stocksFile)
+	return stocksFile;
+}
+
+function build_json(line) {
+	line = line.split(",");
+	var data = {"Stock":line[0]}
+	var lis = [];
+	var ind = 0;
+	for (var i = 1; i < line.length; i += 3) {
+		lis[ind] = [parseInt(line[i + 1]), parseInt(line[i + 2])];
+		ind += 1;
+	}
+	data["stocksFile"] = lis;
+	return data
+}
+
+function find_line(districts, state) {
+	//districts.replace("htt")
+	var lines = districts.split("/symbol");
+	var line = "";
+	for (var i = 0; i < lines.length; i++) {
+		if(lines[i].toLowerCase().includes(state.toLowerCase())){
+			line = lines[i];
+		}
+	}
+	return line;
+}
+
+console.log("Web Service Has Started");
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+	var stock = req.query.stock;
+	if(stock == undefined) {
+		res.status(400);
+		res.send("Missing required parameters");
+		}
+
+	var file_name = "stock337data2.txt";
+	
+	var stockz = read_file(file_name);
+	var line = find_line(stockz, stock);
+
+	if(line == "") {
+		res.status(410);
+		res.send("State was not found");
+		} 
+
+    res.send(JSON.stringify(line));
+})
+
+app.listen(3000);
